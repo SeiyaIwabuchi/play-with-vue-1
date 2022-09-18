c84f29d5d070e0afc16871d1b87b2c562a33fec2
SeiyaIwabuchi <0123ook@gmail.com>
2022-09-18 13:18:09 +0900

    vuex導入

--- znphm1/src/App.vue
+++ znphm1/src/App.vue
@@ -5,59 +5,23 @@
       align-items: center;
       padding: 10px;
     ">
-    <ProductForm @showSnackbar="showSnackbar" @setProductTable="setProductTable" @setProductFormSetter="setProductFormSetter" />
-    <ProductTable @showSnackbar="showSnackbar" @setProductForm="setProductForm" @setProductTableSetter="setProductTableSetter" />
-    <v-snackbar v-model="snackbar.isShow">
-      {{ snackbar.text }}
-
-      <template v-slot:action="{ attrs }">
-        <v-btn color="pink" text v-bind="attrs" @click="snackbar.isShow = false">
-          閉じる
-        </v-btn>
-      </template>
-    </v-snackbar>
+    <ProductForm />
+    <ProductTable />
+    <Snackbar />
   </div>
 </template>
 
 <script>
-import dProduct from "./dProduct";
 import ProductTable from "./ProductTable";
 import ProductForm from "./ProductForm.vue";
+import Snackbar from "./Snackbar.vue";
 export default {
   name: "App",
   components: {
     ProductTable,
-    ProductForm
+    ProductForm,
+    Snackbar
 },
-  data() {
-    return {
-      newProduct: new dProduct("", "", 0),
-      snackbar: { isShow: false, text: "メッセージはありません" },
-      error: false,
-      rules: [
-        value => {
-          this.error = value.length < 1;
-          return !!value || 'Required.';
-        }
-      ],
-      setProductTable: function (){},
-      setProductForm: function (){},
-    };
-  },
-  methods: {
-    // setterのsetterは意味が分からなすぎるので、
-    // https://se-tomo.com/2018/11/03/vue-js-%E3%82%B3%E3%83%B3%E3%83%9D%E3%83%BC%E3%83%8D%E3%83%B3%E3%83%88%E9%96%93%E3%81%AE%E9%80%9A%E4%BF%A1/
-    // イベントバス
-    setProductTableSetter: function (productsSetter) {
-      this.setProductTable = productsSetter;
-    },
-    showSnackbar: function (text) {
-      this.snackbar.text = text;
-      this.snackbar.isShow = true;
-    },
-    setProductFormSetter: function (productFormSetter) {
-      this.setProductForm = productFormSetter;
-    },
-  },
+  
 };
 </script>
--- znphm1/src/Product.vue
+++ znphm1/src/Product.vue
@@ -4,26 +4,61 @@
     <td>{{ product.name }}</td>
     <td>{{ product.price }}</td>
     <td>
-      <v-btn v-on:click="edit" style="margin: 10px">編集</v-btn>
-      <v-btn v-on:click="remove">削除</v-btn>
+      <v-btn @click="edit" style="margin: 10px">編集</v-btn>
+      <v-btn @click="remove">削除</v-btn>
     </td>
   </tr>
 </template>
 
 <script>
+import dProduct from "./dProduct";
+import { serverUrl } from "./config";
 export default {
   name: "Product",
-  props: ["product", "events"],
+  props: ["product"],
+  computed: {
+    products() {
+      return this.$store.getters.products;
+    }
+  },
   methods: {
-    remove: function () {
-      this.$emit("remove", this.product);
+    setProductTable(products) {
+      this.$store.commit("setProducts", products);
     },
-    edit: function () {
-      this.$emit("edit", this.product);
+    showSnackbar(text) {
+      this.$store.commit("showSnackbar", text);
+    },
+    setProductForm(product) {
+      this.$store.commit("setProductForm", product);
+    },
+    remove: async function () {
+
+      // 最新のデータを取得する
+      let data = await (await fetch(serverUrl)).json();
+      this.setProductTable(Array.from(data).map(p => new dProduct(p.id, p.name, p.price)));
+
+      await fetch(`${serverUrl}/${this.product.code}`, { method: "delete" });
+
+      // 削除後のデータを取得する
+      data = await (await fetch(serverUrl)).json();
+      this.setProductTable(Array.from(data).map(p => new dProduct(p.id, p.name, p.price)));
+
+      this.showSnackbar("削除しました");
+    },
+
+    edit: async function () {
+
+      // 最新のデータを取得する
+      let data = await (await fetch(serverUrl)).json();
+      this.setProductTable(Array.from(data).map(p => new dProduct(p.id, p.name, p.price)));
+
+      // オブジェクトのcode(id)をもとにフォームにセットする
+      this.setProductForm(this.products.find(p => p.code === this.product.code));
     },
   },
 };
 </script>
 
 <style>
+
 </style>
